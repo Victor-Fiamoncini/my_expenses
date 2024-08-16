742e4c7cf2c09c2b977f5c83b96a3f7881b92ee9
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-08-16 01:44:34 -0300

    refactor: moves category_options to expenses_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-  def category_options
-    [
-      ['Bill', :bill],
-      ['Food', :food],
-      ['Rent', :rent],
-      ['Subscription', :subscription],
-      ['Transportation', :transportation],
-      ['Utilities', :utilities]
-    ]
-  end
 end
--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,4 +1,15 @@
 module ExpensesHelper
+  def category_options
+    [
+      ['Bill', :bill],
+      ['Food', :food],
+      ['Rent', :rent],
+      ['Subscription', :subscription],
+      ['Transportation', :transportation],
+      ['Utilities', :utilities]
+    ]
+  end
+
   def number_to_currency_br(number)
     number_to_currency(number, unit: 'R$ ', separator: ',', delimiter: '.')
   end
