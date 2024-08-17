881cfc6ebe3a506d9ef93871f3a8c0d5b66252c3
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-08-17 17:43:03 -0300

    feat: permits only future payment dates to be registered in expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -15,4 +15,14 @@ class Expense < ApplicationRecord
   validates :payment_date, presence: true
   validates :user_id, presence: true
   validates :value, numericality: { greater_than: 0 }, presence: true
+
+  validate :payment_date_cannot_be_in_the_past
+
+  private
+
+    def payment_date_cannot_be_in_the_past
+      return unless payment_date.present? && payment_date < Date.today
+
+      errors.add(:payment_date, "Payment date can't be in the past")
+    end
 end
