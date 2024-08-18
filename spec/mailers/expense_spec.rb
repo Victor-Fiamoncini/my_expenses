b6c14c5199837349b30a5ed717e668f0c7103e1b
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-08-18 02:23:54 -0300

    test: adds expense mailer tests

---
+++ spec/mailers/expense_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe ExpenseMailer, type: :mailer do
+  let(:user) do
+    User.create(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: Faker::Alphanumeric.alphanumeric(number: 10)
+    )
+  end
+
+  let(:expense) do
+    Expense.new(
+      category: :bill,
+      name: Faker::Name.name,
+      payment_date: Date.today + 30,
+      value: Faker::Number.number(digits: 3),
+      user_id: user.id
+    )
+  end
+
+  describe 'payment_day_is_tomorrow' do
+    let(:mail) do
+      described_class.with(expense: expense).payment_day_is_tomorrow
+    end
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq "The payment day of your expense: #{expense.name} is tomorrow"
+      expect(mail.to).to eq [expense.user.email]
+      expect(mail.content_type).to eq 'text/html; charset=UTF-8'
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to include(expense.name)
+    end
+  end
+end
