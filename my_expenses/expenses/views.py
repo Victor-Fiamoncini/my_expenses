6fb6f37cbd31d93ab5ade36be11c24802dbb2c7c
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-09-01 02:06:06 -0300

    feat: adds expense update view

--- my_expenses/expenses/views.py
+++ my_expenses/expenses/views.py
@@ -9,10 +9,11 @@ from . import models
 
 @login_required
 def index(request: HttpRequest) -> HttpResponse:
+    page_number = request.GET.get("page")
+
     expenses = request.user.expenses.all().order_by("-created_at")  # type: ignore
 
     paginator = Paginator(expenses, 10)
-    page_number = request.GET.get("page")
     pagination = paginator.get_page(page_number)
 
     return render(request, "expenses/index.html", {"pagination": pagination})
@@ -41,3 +42,25 @@ def create(request: HttpRequest) -> HttpResponse:
         form = forms.CreateExpenseForm()
 
     return render(request, "expenses/create.html", {"form": form})
+
+
+@login_required
+def update(request: HttpRequest, id: int) -> HttpResponse:
+    expense = models.Expense.objects.get(id=id)
+
+    if request.method == "POST":
+        form = forms.UpdateExpenseForm(request.POST, instance=expense)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("expenses:index")
+    else:
+        form = forms.UpdateExpenseForm(instance=expense)
+
+    return render(request, "expenses/update.html", {"expense": expense, "form": form})
+
+
+@login_required
+def delete(request: HttpRequest, id: int) -> HttpResponse:
+    return render(request, "expenses/update.html")
