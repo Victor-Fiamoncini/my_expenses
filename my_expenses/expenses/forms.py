87f4dcd39fe5ad059e2c3edcf04a4c8d0ddf0768
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-09-01 02:05:31 -0300

    feat: adds update expense form class and UI & adds update and delete urls

--- my_expenses/expenses/forms.py
+++ my_expenses/expenses/forms.py
@@ -1,7 +1,46 @@
-from django.forms import CharField, DateField, DateInput, Form, TextInput
+from django.forms import CharField, DateField, DateInput, Form, ModelForm, TextInput
+
+from . import models
 
 
 class CreateExpenseForm(Form):
+    name = CharField(
+        widget=TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "A name that makes sense with the expense",
+            },
+        ),
+    )
+
+    value = CharField(
+        widget=TextInput(
+            attrs={
+                "id": "expense-value-input",
+                "class": "form-control",
+                "placeholder": "A numeric value",
+            },
+        ),
+    )
+
+    payment_date = DateField(
+        input_formats=["%d/%m/%Y"],
+        widget=DateInput(
+            format="%d/%m/%Y",
+            attrs={
+                "class": "form-control",
+                "type": "date",
+                "placeholder": "The payment date",
+            },
+        ),
+    )
+
+
+class UpdateExpenseForm(ModelForm):
+    class Meta:
+        model = models.Expense
+        fields = ["name", "value", "payment_date"]
+
     name = CharField(
         widget=TextInput(
             attrs={
--- my_expenses/expenses/urls.py
+++ my_expenses/expenses/urls.py
@@ -7,4 +7,6 @@ app_name = "expenses"
 urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
+    path("<int:id>/update", views.update, name="update"),
+    path("<int:id>/delete", views.delete, name="delete"),
 ]
