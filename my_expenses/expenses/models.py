9598e2900d199dc8caa1e6fe09d46ead58a071a8
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-08-31 23:45:33 -0300

    refactor: removes category from expenses model

--- my_expenses/expenses/models.py
+++ my_expenses/expenses/models.py
@@ -8,23 +8,12 @@ from django.db.models import (
     DecimalField,
     ForeignKey,
     Model,
-    TextChoices,
 )
 from django.utils import timezone
 
 
-class Category(TextChoices):
-    BILL = "BILL", "Bill"
-    FOOD = "FOOD", "Food"
-    RENT = "RENT", "Rent"
-    SUBSCRIPTION = "SUBSCRIPTION", "Subscription"
-    TRANSPORTATION = "TRANSPORTATION", "Transportation"
-    UTILITIES = "UTILITIES", "Utilities"
-
-
 class Expense(Model):
     name = CharField(max_length=150)
-    category = CharField(max_length=50, choices=Category.choices, default=Category.BILL)
     value = DecimalField(max_digits=10, decimal_places=2)
     payment_date = DateField()
     user = ForeignKey(
