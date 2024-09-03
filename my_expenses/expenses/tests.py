501f06be0284fe66a624e2a228f087d2d2fc19be
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-09-03 01:25:41 -0300

    test: adds base expenses model unit tests

--- my_expenses/expenses/tests.py
+++ my_expenses/expenses/tests.py
@@ -1,3 +1,53 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from faker import Faker
 
-# Create your tests here.
+from . import models
+
+
+class ExpenseModalTestCase(TestCase):
+    def setUp(self) -> None:
+        self.faker = Faker()
+
+        self.user = get_user_model().objects.create_user(
+            username=self.faker.user_name(),
+            email=self.faker.email(),
+            password=self.faker.password(),
+        )
+
+    def test_must_create_an_expense_with_valid_data(self) -> None:
+        name = self.faker.name()
+        value = str(self.faker.pyfloat(left_digits=3, right_digits=2, positive=True))
+        payment_date = self.faker.future_date()
+
+        expense = models.Expense(
+            name=name, value=value, payment_date=payment_date, user=self.user
+        )
+        expense.save()
+
+        self.assertEqual(models.Expense.objects.get(name=name), expense)
+
+    def test_must_raise_an_validation_error_if_payment_date_is_invalid(self) -> None:
+        name = self.faker.name()
+        value = str(self.faker.pyfloat(left_digits=3, right_digits=2, positive=True))
+        payment_date = self.faker.past_date()
+
+        expense = models.Expense(
+            name=name, value=value, payment_date=payment_date, user=self.user
+        )
+
+        with self.assertRaises(ValidationError):
+            expense.save()
+
+    def test_must_raise_an_validation_error_if_value_is_invalid(self) -> None:
+        name = self.faker.name()
+        value = str(self.faker.pyfloat(left_digits=3, right_digits=3, positive=True))
+        payment_date = self.faker.future_date()
+
+        expense = models.Expense(
+            name=name, value=value, payment_date=payment_date, user=self.user
+        )
+
+        with self.assertRaises(ValidationError):
+            expense.save()
