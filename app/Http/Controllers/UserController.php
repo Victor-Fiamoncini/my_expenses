8cae3d392c2ddffbf246b0e3734e5e9bb95f84b0
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-10-06 19:28:09 -0300

    feat: adds base UserController with register handler

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\View\View;
+
+class UserController extends Controller
+{
+    public function create(): View
+    {
+        return view('user.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:users,email',
+            'name' => 'required|string|max:255',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        User::create([
+            'email' => $request->email,
+            'name' => $request->name,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('session.create')->with('success', 'Registration successful!');
+    }
+}
