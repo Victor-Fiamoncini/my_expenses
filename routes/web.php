487c84d5e05b0d7422688fe917e9ca296bca3973
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-10-10 19:10:03 -0300

    feat: adds base SessionController with web routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\SessionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('users', UserController::class)->only(['create', 'store']);
+
+Route::resource('sessions', SessionController::class)->only(['create', 'store']);
+
+Route::delete('/sessions', [SessionController::class, 'destroy'])->name('sessions.destroy');
