d30969710dce91e2c2330684b78ef8cf471fb4b7
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-10-23 21:42:12 -0300

    feat: adds expense belongs-to user model association

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Expense extends Model
 {
@@ -20,4 +21,12 @@ class Expense extends Model
         'payment_date',
         'user_id',
     ];
+
+    /**
+     * Get the expense user
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
