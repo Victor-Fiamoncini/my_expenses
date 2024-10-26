bc3c70644f8f301a63ba4bfccc0d1b85c5ea0989
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-10-26 13:43:31 -0300

    feat: adds StoreUserRequest for UserController::store

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
 
 class UserController extends Controller
@@ -16,27 +15,18 @@ class UserController extends Controller
         return view('user.create');
     }
 
-    public function store(Request $request): RedirectResponse
+    public function store(StoreUserRequest $request): RedirectResponse
     {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email|unique:users,email',
-            'name' => 'required|string|max:255',
-            'password' => 'required|string|min:12|confirmed',
-        ]);
-
-        if ($validator->fails()) {
-            return back()->withErrors($validator)->withInput();
-        }
+        $payload = $request->safe()->only(['email', 'name', 'password']);
 
         User::create([
-            'email' => $request->email,
-            'name' => $request->name,
-            'password' => Hash::make($request->password),
+            'email' => $payload['email'],
+            'name' =>  $payload['name'],
+            'password' => Hash::make($payload['password']),
         ]);
 
-        return redirect()->route('sessions.create')->with(
-            'success',
-            "$request->name, your registration was completed!"
-        );
+        return redirect()
+            ->route('sessions.create')
+            ->with('success', "$request->name, your registration was completed!");
     }
 }
