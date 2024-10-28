424c172b513a86df2fafd41e86ccfd49f050776d
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-10-27 21:25:17 -0300

    feat: adds update/delete routes and uses Gate facade to authorize delete/view/update ExpenseController methods

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreExpenseRequest;
+use App\Http\Requests\UpdateExpenseRequest;
 use App\Models\Expense;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
 
 class ExpenseController extends Controller
@@ -14,7 +15,7 @@ class ExpenseController extends Controller
     public function index(): View
     {
         $expenses = Expense::query()
-            ->where('user_id', Auth::user()->id)
+            ->where('user_id', Auth::id())
             ->orderByDesc('created_at')
             ->paginate(10);
 
@@ -30,9 +31,36 @@ class ExpenseController extends Controller
     {
         $payload = $request->safe()->only(['name', 'value', 'payment_date']);
 
-        $expense = new Expense([...$payload, 'user_id' => Auth::user()->id]);
+        $expense = new Expense([...$payload, 'user_id' => Auth::id()]);
         $expense->save();
 
         return redirect()->route('expenses.index')->with('success', "Expense {$payload['name']} has been created");
+    }
+
+    public function edit(Expense $expense): View
+    {
+        Gate::authorize('view', $expense);
+
+        return view('expense.edit', compact('expense'));
+    }
+
+    public function update(UpdateExpenseRequest $request, Expense $expense): RedirectResponse
+    {
+        Gate::authorize('update', $expense);
+
+        $payload = $request->safe()->only(['name', 'value', 'payment_date', 'paid']);
+
+        $expense->update($payload);
+
+        return redirect()->route('expenses.index')->with('success', "Expense {$payload['name']} has been updated");
+    }
+
+    public function destroy(Expense $expense): RedirectResponse
+    {
+        Gate::authorize('delete', $expense);
+
+        $expense->delete();
+
+        return redirect()->route('expenses.index')->with('success', "Expense $expense->name has been deleted");
     }
 }
