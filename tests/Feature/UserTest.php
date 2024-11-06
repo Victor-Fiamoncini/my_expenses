0652d6d076a6aef4789da7aebbeefba81c63b514
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-11-05 23:28:02 -0300

    test: adds user feature unit test

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public function test_it_creates_an_user(): void
+    {
+        $name = $this->faker()->name();
+        $email = $this->faker()->email();
+        $password = $this->faker()->password(minLength: 12);
+        $phone = $this->faker()->phoneNumber();
+
+        $response = $this->post(
+            uri: route('users.store'),
+            data: [
+                'name' => $name,
+                'email' => $email,
+                'password' => $password,
+                'password_confirmation' => $password,
+                'phone' => $phone,
+            ]
+        );
+
+        $response->assertRedirect(route('sessions.create'));
+        $response->assertSessionHas('success');
+
+        $this->assertDatabaseHas('users', ['email' => $email]);
+    }
+}
