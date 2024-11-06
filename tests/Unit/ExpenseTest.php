13e4fed691391362d5ad9ebd612efba79bb58d2a
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-11-05 23:09:38 -0300

    refactor: uses contructor instead of factory on expense unit test

--- tests/Unit/ExpenseTest.php
+++ tests/Unit/ExpenseTest.php
@@ -12,21 +12,24 @@ class ExpenseTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
-    public function test_it_calculates_total_expense_value(): void
+    public function test_it_creates_an_expense(): void
     {
         $name = $this->faker()->name();
         $paymentDate = $this->faker()->dateTime();
+        $userId = User::factory()->create()->id;
         $value = $this->faker->randomFloat(2);
 
-        $expense = Expense::factory()->create([
+        $expense = new Expense([
             'name' => $name,
             'payment_date' => $paymentDate,
-            'user_id' => User::factory(),
+            'user_id' => $userId,
             'value' => $value,
         ]);
+        $expense->save();
 
         $this->assertEquals($name, $expense->name);
         $this->assertEquals($paymentDate, $expense->payment_date);
+        $this->assertEquals($userId, $expense->user->id);
         $this->assertEquals($value, $expense->value);
     }
 }
