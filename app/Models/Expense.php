22a7695c7052de14eccee456ac890596de2c043e
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-11-09 23:07:25 -0300

    feat: adds new type attr to Expense model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -6,15 +6,36 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * Class Expense
+ *
+ * @property int $id
+ * @property string $name
+ * @property float $value
+ * @property string $payment_date
+ * @property string $paid
+ * @property string $type
+ * @property string $user_id
+ * @property string $created_at
+ * @property string $updated_at
+ * @property-read \App\Models\User $user
+ *
+ * @mixin \Eloquent
+ */
 class Expense extends Model
 {
     use HasFactory;
+
+    const MONTHLY = 'MONTHLY';
+
+    const SINGLE = 'SINGLE';
 
     protected $fillable = [
         'name',
         'value',
         'payment_date',
         'paid',
+        'type',
         'user_id',
     ];
 
@@ -25,5 +46,15 @@ class Expense extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function isMonthly(): bool
+    {
+        return $this->type === Expense::MONTHLY;
+    }
+
+    public function isSingle(): bool
+    {
+        return $this->type === Expense::SINGLE;
     }
 }
