f24b258fbad084ff71a8a5ca62a837ce45cc3f72
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-11-10 21:30:06 -0300

    feat: adds type field to expenses create form

--- resources/views/expense/create.blade.php
+++ resources/views/expense/create.blade.php
@@ -12,40 +12,38 @@
             <div class="mb-4">
                 <label class="form-label" for="name">Nome:</label>
 
-                <input
-                    id="name"
-                    class="form-control"
-                    type="text"
-                    name="name"
-                    value="{{ old('name') }}"
-                    placeholder="Um nova que tenha relação com a despesa"
-                />
+                <input id="name" class="form-control" type="text" name="name" value="{{ old('name') }}"
+                    placeholder="Um nova que tenha relação com a despesa" />
             </div>
 
             <div class="mb-4">
                 <label class="form-label" for="value">Valor:</label>
 
-                <input
-                    id="value"
-                    class="form-control"
-                    type="text"
-                    name="value"
-                    value="{{ old('value') }}"
-                    placeholder="Um valor numérico"
-                />
+                <input id="value" class="form-control" type="text" name="value" value="{{ old('value') }}"
+                    placeholder="Um valor numérico" />
+            </div>
+
+            <div class="mb-4">
+                <label class="form-label" for="payment-date">Data de pagamento:</label>
+
+                <input id="payment-date" class="form-control" type="text" name="payment_date"
+                    value="{{ old('payment_date') }}" placeholder="Quando irá pagar/quitar" />
             </div>
 
             <div class="mb-5">
-                <label class="form-label" for="payment-date">Data de Pagamento:</label>
+                <p>Essa despesa será paga de forma:</p>
 
-                <input
-                    id="payment-date"
-                    class="form-control"
-                    type="text"
-                    name="payment_date"
-                    value="{{ old('payment_date') }}"
-                    placeholder="A em que irá pagar/quitar"
-                />
+                <div class="btn-group" role="group" aria-label="Expense Type Group">
+                    <input id="type-single" class="btn-check" name="type" type="radio" autocomplete="off"
+                        value="SINGLE" @if (old('type') === 'SINGLE' || old('type') === null) checked @endif />
+
+                    <label class="btn btn-sm btn-outline-primary" for="type-single">Avulsa</label>
+
+                    <input id="type-monthly" class="btn-check" name="type" type="radio" autocomplete="off"
+                        value="MONTHLY" @if (old('type') === 'MONTHLY') checked @endif />
+
+                    <label class="btn btn-sm btn-outline-primary" for="type-monthly">Mensal</label>
+                </div>
             </div>
 
             <div class="d-flex justify-content-start align-items-center gap-2">
