01d21489e702f4f87275e684b1ab997982bab0c8
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-12-07 23:37:45 -0300

    feat: adds installments to database seeder

---
+++ database/factories/InstallmentFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Installment>
+ */
+class InstallmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'value' => fake()->randomFloat(2, 10, 1000),
+            'paid' => fake()->boolean(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Expense;
+use App\Models\Installment;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(ExpenseSeeder::class);
+        $user = User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password1234'),
+        ]);
+
+        Expense::factory()
+            ->count(2)
+            ->has(
+                Installment::factory()
+                    ->count(5)
+                    ->state(new Sequence(['paid' => false, 'value' => 119.998])),
+                'installments'
+            )
+            ->create([
+                'number_of_installments' => 5,
+                'type' => Expense::IN_INSTALLMENTS,
+                'user_id' => $user->id,
+                'value' => 599.99,
+            ]);
     }
 }
